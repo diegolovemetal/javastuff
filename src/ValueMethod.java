623e6c92a0d62c1e2b41fc325712903d8d0163b8
diegolovemetal <2015211713@stu.cqupt.edu.cn>
2019-06-10 14:24:24 +0800

    xxxValue method

---
+++ src/ValueMethod.java
@@ -0,0 +1,14 @@
+public class ValueMethod {
+    public static void main(String[] args) {
+        Integer x = 5;
+
+        //返回byte原始数据类型
+        System.out.println(x.byteValue());
+
+        //double
+        System.out.println(x.doubleValue());
+
+        //long
+        System.out.println(x.longValue());
+    }
+}
