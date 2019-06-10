6c5225567d56f3a9f4b1f9a52d7e163a9594b29a
diegolovemetal <2015211713@stu.cqupt.edu.cn>
2019-06-10 08:06:55 +0800

    WHile Test

--- src/WhileTest.java
+++ src/WhileTest.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class WhileTest {
+    public static void main(String[] args) {
+        List<String> veggies = new ArrayList<>();
+        veggies.add("Bean");
+        veggies.add("Leek");
+        veggies.add("onion");
+
+        Iterator<String> it = veggies.iterator();
+
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
 }
